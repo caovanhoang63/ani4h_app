16af0b7255b0f2e50698a64d540c47cfe715e417
Hung Mai <126455648+mhhung0811@users.noreply.github.com>
2025-06-10 07:49:49 +0700

    add: map navigate for some page

--- lib/features/favorite/presentation/ui/widget/favorite_card.dart
+++ lib/features/favorite/presentation/ui/widget/favorite_card.dart
@@ -1,7 +1,10 @@
+import 'package:ani4h_app/common/provider/current_movie_state/current_movie_controller.dart';
+import 'package:ani4h_app/core/route/route_name.dart';
 import 'package:ani4h_app/features/favorite/domain/model/favorite_model.dart';
 import 'package:ani4h_app/features/favorite/presentation/controller/favorite_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 
 class FavoriteCard extends ConsumerWidget {
@@ -12,109 +15,115 @@ class FavoriteCard extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final cardHeight = 80.0;
 
-    return Container(
-      height: cardHeight,
-      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 4),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          //Image
-          ClipRRect(
-            borderRadius: BorderRadius.circular(12),
-            child: Image.network(
-              item.imageUrl,
-              width: cardHeight*1.5,
-              height: cardHeight,
-              fit: BoxFit.cover,
-              loadingBuilder: (context, child, loadingProgress) {
-                if (loadingProgress == null) return child;
-                return Container(
-                  color: Colors.grey[300],
-                  height: cardHeight,
-                  child: const Center(child: CircularProgressIndicator()),
-                );
-              },
-              errorBuilder: (context, error, stackTrace) {
-                return Container(
-                  color: Colors.grey[300],
-                  height: cardHeight,
-                  child: const Center(
-                    child: Icon(Icons.broken_image, color: Colors.white, size: 50),
-                  ),
-                );
-              },
-            ),
-          ),
-
-          //Space
-          const SizedBox(width: 8),
-
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  item.title,
-                  overflow: TextOverflow.ellipsis,
-                  style: const TextStyle(
-                    fontSize: 16,
-                    color: Colors.white,
-                  ),
-                ),
-                Row(
-                  children: [
-                    Text(
-                      item.avgStar.toString(),
-                      style: const TextStyle(
-                        fontSize: 14,
-                        color: Colors.grey,
-                      ),
+    return GestureDetector(
+      onTap: () {
+        ref.read(currentMovieControllerProvider.notifier).fetchCurrentMovie(item.id);
+        context.replaceNamed(movieDetailRoute);
+      },
+      child: Container(
+        height: cardHeight,
+        margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 4),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            //Image
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.network(
+                item.imageUrl,
+                width: cardHeight*1.5,
+                height: cardHeight,
+                fit: BoxFit.cover,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress == null) return child;
+                  return Container(
+                    color: Colors.grey[300],
+                    height: cardHeight,
+                    child: const Center(child: CircularProgressIndicator()),
+                  );
+                },
+                errorBuilder: (context, error, stackTrace) {
+                  return Container(
+                    color: Colors.grey[300],
+                    height: cardHeight,
+                    child: const Center(
+                      child: Icon(Icons.broken_image, color: Colors.white, size: 50),
                     ),
-                    const SizedBox(width: 4),
-                    Icon(Icons.star, color: Colors.yellow[700], size: 16),
-                  ],
-                ),
-                Row(
-                  children: item.genres.take(3).map((genre) {
-                    return Container(
-                      margin: const EdgeInsets.only(right: 8, top: 4),
-                      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                      decoration: BoxDecoration(
-                        color: Colors.grey[800],
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: Text(
-                        genre.name,
+                  );
+                },
+              ),
+            ),
+
+            //Space
+            const SizedBox(width: 8),
+
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    item.title,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        item.avgStar.toString(),
                         style: const TextStyle(
-                          fontSize: 10,
-                          color: Colors.white,
+                          fontSize: 14,
+                          color: Colors.grey,
                         ),
                       ),
-                    );
-                  }).toList(),
-                )
-              ],
+                      const SizedBox(width: 4),
+                      Icon(Icons.star, color: Colors.yellow[700], size: 16),
+                    ],
+                  ),
+                  Row(
+                    children: item.genres.take(3).map((genre) {
+                      return Container(
+                        margin: const EdgeInsets.only(right: 8, top: 4),
+                        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                        decoration: BoxDecoration(
+                          color: Colors.grey[800],
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Text(
+                          genre.name,
+                          style: const TextStyle(
+                            fontSize: 10,
+                            color: Colors.white,
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                  )
+                ],
+              ),
             ),
-          ),
 
-          SizedBox(
-            width: 24,
-            height: 24,
-            child: IconButton(
-                padding: EdgeInsets.zero,
-                onPressed: () {
-                  ref.read(favoriteControllerProvider.notifier).removeFavorite(item.id);
-                },
-                icon: Icon(Icons.remove_circle_outline, color: Colors.grey, size: 24)
-            ),
-          )
-        ],
-      ),
+            SizedBox(
+              width: 24,
+              height: 24,
+              child: IconButton(
+                  padding: EdgeInsets.zero,
+                  onPressed: () {
+                    ref.read(favoriteControllerProvider.notifier).removeFavorite(item.id);
+                  },
+                  icon: Icon(Icons.remove_circle_outline, color: Colors.grey, size: 24)
+              ),
+            )
+          ],
+        ),
+      )
     );
   }
 }
--- lib/features/history/presentation/ui/widget/history_card.dart
+++ lib/features/history/presentation/ui/widget/history_card.dart
@@ -1,6 +1,9 @@
+import 'package:ani4h_app/common/provider/current_movie_state/current_movie_controller.dart';
+import 'package:ani4h_app/core/route/route_name.dart';
 import 'package:ani4h_app/features/history/domain/model/history_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class HistoryCard extends ConsumerWidget {
   final HistoryModel item;
@@ -15,88 +18,94 @@ class HistoryCard extends ConsumerWidget {
         ? (item.watchedDuration / item.duration).clamp(0.0, 1.0)
         : 0.0;
 
-    return Container(
-      height: cardHeight,
-      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 4),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          //Image
-          ClipRRect(
-            borderRadius: BorderRadius.circular(8),
-            child: Stack(
-              children: [
-                Image.network(
-                  item.imageUrl,
-                  width: cardHeight*1.5,
-                  height: cardHeight,
-                  fit: BoxFit.cover,
-                  loadingBuilder: (context, child, loadingProgress) {
-                    if(loadingProgress == null) return child;
-                    return Container(
-                      color: Colors.grey[300],
+    return GestureDetector(
+      onTap: () {
+        ref.read(currentMovieControllerProvider.notifier).fetchCurrentMovie(item.id);
+        context.replaceNamed(movieDetailRoute);
+      },
+      child: Container(
+        height: cardHeight,
+        margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 4),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            //Image
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: Stack(
+                  children: [
+                    Image.network(
+                      item.imageUrl,
+                      width: cardHeight*1.5,
                       height: cardHeight,
-                      child: const Center(child: CircularProgressIndicator()),
-                    );
-                  },
-                  errorBuilder: (context, error, stackTrace) {
-                    return Container(
-                      color: Colors.grey[300],
-                      height: cardHeight,
-                      child: const Center(
-                        child: Icon(Icons.broken_image, color: Colors.white, size: 50),
-                      ),
-                    );
-                  },
-                ),
+                      fit: BoxFit.cover,
+                      loadingBuilder: (context, child, loadingProgress) {
+                        if(loadingProgress == null) return child;
+                        return Container(
+                          color: Colors.grey[300],
+                          height: cardHeight,
+                          child: const Center(child: CircularProgressIndicator()),
+                        );
+                      },
+                      errorBuilder: (context, error, stackTrace) {
+                        return Container(
+                          color: Colors.grey[300],
+                          height: cardHeight,
+                          child: const Center(
+                            child: Icon(Icons.broken_image, color: Colors.white, size: 50),
+                          ),
+                        );
+                      },
+                    ),
 
-                //Progress bar
-                Positioned(
-                  bottom: 0,
-                  left: 0,
-                  right: 0,
-                  child: LinearProgressIndicator(
-                    value: progress,
-                    minHeight: 4,
-                    backgroundColor: Colors.grey,
-                    valueColor: const AlwaysStoppedAnimation<Color>(Colors.redAccent),
+                    //Progress bar
+                    Positioned(
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                      child: LinearProgressIndicator(
+                        value: progress,
+                        minHeight: 4,
+                        backgroundColor: Colors.grey,
+                        valueColor: const AlwaysStoppedAnimation<Color>(Colors.redAccent),
+                      ),
+                    ),
+                  ]
+              ),
+            ),
+
+            //Space
+            const SizedBox(width: 8),
+
+            //Name & Nation & tags
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '${item.title} - Tập ${item.episodeNumber}',
+                  style: const TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
                   ),
                 ),
-              ]
+                Text(
+                  item.viewCount > 0
+                      ? '${item.viewCount} lượt xem'
+                      : 'Chưa có lượt xem',
+                  style: const TextStyle(
+                    fontSize: 14,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
             ),
-          ),
-
-          //Space
-          const SizedBox(width: 8),
-
-          //Name & Nation & tags
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                '${item.title} - Tập ${item.episodeNumber}',
-                style: const TextStyle(
-                  fontSize: 16,
-                  color: Colors.white,
-                ),
-              ),
-              Text(
-                item.viewCount > 0
-                    ? '${item.viewCount} lượt xem'
-                    : 'Chưa có lượt xem',
-                style: const TextStyle(
-                  fontSize: 14,
-                  color: Colors.grey,
-                ),
-              ),
-            ],
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
